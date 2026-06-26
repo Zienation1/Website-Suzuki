8d61f8c9ee0cfa19f2e15b92ee32e9297b018565
Zienation <rofifdhaniswarakenzie@gmail.com>
2026-06-26 19:56:51 +0700

    Reduce connectionLimit to 2 to avoid max_user_connections limit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const db = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 5,
+    connectionLimit: 2,
     queueLimit: 0
 });
 
